7c94dd8d99f52aa54890b78286d83230d04277f8
Beñat Barrueta <benat.barrueta@opendeusto.es>
2024-02-07 10:51:54 +0100

    Initial Commit

---
+++ AppTiempo/AppTiempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTiempoApp.swift
+//  AppTiempo
+//
+//  Created by Beñat Barrueta on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTiempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
